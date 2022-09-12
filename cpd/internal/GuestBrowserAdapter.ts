c1b1a754a383c701226c3383576d13512d281a0c
Alexey Chuprin <a.chuprin@capjack.ru>
2022-09-12 20:19:11 +0300

    Develop

--- cpd/internal/GuestBrowserAdapter.ts
+++ cpd/internal/GuestBrowserAdapter.ts
@@ -7,15 +7,13 @@ export class GuestBrowserAdapter extends AbstractBrowserAdapter {
 	public readonly purchaseAvailable: boolean = false
 	
 	public authorize(): Promise<CpdAccount | null> {
-		return Promise.resolve(new CpdAccount(
-			this.makeCsiAuthKeyPrefix() + this.getDeviceId(),
-			null,
-			null
-		))
-	}
-	
-	protected makeCsiAuthKeyPrefix(): string {
-		return 'gu' + super.makeCsiAuthKeyPrefix()
+		return this.getDeviceId().then(deviceId => {
+			return new CpdAccount(
+				this.makeCsiAuthKeyPrefix() + deviceId,
+				null,
+				null
+			)
+		})
 	}
 	
 	public loadShop(ids: string[], receiver: (currency: string, products: Array<{id: string; price: number}>) => void): void {
@@ -24,5 +22,9 @@ export class GuestBrowserAdapter extends AbstractBrowserAdapter {
 	
 	public purchase(product: {id: string; name: string; price: number}, onSuccess: (orderId: string, receipt: string) => void, onFail: (reason: string) => void): void {
 		throw new UnsupportedOperationException()
+	}
+	
+	protected makeCsiAuthKeyPrefix(): string {
+		return 'gu' + super.makeCsiAuthKeyPrefix()
 	}
 }
